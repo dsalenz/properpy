2dda82766c86caa84d1ce510ddf54c4f019aeca6
Dennis Salenz <d.salenz@googlemail.com>
2017-07-25 21:36:41 +0200

    add not_search_cartridge variable for better configuration

--- proper.py
+++ proper.py
@@ -5,9 +5,8 @@ import subprocess
 
 # VARIABLES to set
 
-triumph_working_dir_path = '/Users/salenz/git/triumph/demandware-frontend/cartridges/triumph_storefront/cartridge/templates/resources'
-sloggi_working_dir_path = '/Users/salenz/git/triumph/demandware-frontend/cartridges/sloggi_storefront/cartridge/templates/resources'
-
+working_dir_path = '/Users/salenz/git/triumph/demandware-frontend/cartridges/triumph_storefront/cartridge/templates/resources'
+not_search_cartridge = 'sloggi_storefront'
 country_codes = ['de', 'de_CH', 'de_AT', 'fr', 'fr_CH', 'fr_BE', 'sv', 'pl_PL', 'nl', 'nl_BE', 'it_IT', 'it_CH', 'da', 'fi', 'en', 'en_GB']
 
 logfile = open('/Users/salenz/git/triumph/demandware-frontend/properpy_log/log.txt', 'w')
@@ -21,7 +20,7 @@ def is_default_property_file(property_file):
     return not regex.search(property_file)
 
 
-def is_real_property (property_file_line):
+def is_real_property(property_file_line):
     if property_file_line.startswith('#') or property_file_line.startswith('\n') or '=' not in property_file_line:
         return False
     else:
@@ -31,7 +30,7 @@ def is_real_property (property_file_line):
 def find_property_keys(default_property_paths):
     property_keys = []
     for x in default_property_paths:
-        full_property_file_path = triumph_working_dir_path + '/' + x
+        full_property_file_path = working_dir_path + '/' + x
         with open(full_property_file_path) as f:
             print(f)
             try:
@@ -62,7 +61,7 @@ def find_unreferenced_keys(property_keys):
 def is_key_referenced(property_key):
     # search for a specific property_key in all files in all cartridges
     search_path = '/Users/salenz/git/triumph/demandware-frontend/cartridges/'
-    excluded_cartridge_paths = ['sloggi_storefront', '.DS_Store', 'bc_serviceframework']
+    excluded_cartridge_paths = [not_search_cartridge, '.DS_Store', 'bc_serviceframework']
     working_cartridge_paths = os.listdir(search_path)
     for excluded_cartridge_path in excluded_cartridge_paths:
         working_cartridge_paths = [cartridge_path for cartridge_path in working_cartridge_paths if excluded_cartridge_path not in cartridge_path]
@@ -80,7 +79,7 @@ def is_key_referenced_in_path(path, key):
 # imperative Code
 
 # default language
-all_properties_file_paths = os.listdir(triumph_working_dir_path)
+all_properties_file_paths = os.listdir(working_dir_path)
 
 default_property_paths = [path for path in all_properties_file_paths if is_default_property_file(path)]
 default_property_paths = [path for path in default_property_paths if path.endswith('.properties')]
